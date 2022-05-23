da1e17ebbcb39242b9cd7100cc91462ef4c3fdfa
root <4905@holbertonschool.com>
2022-05-23 10:29:11 -0700

    Replaced char variable with int on 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-	char i;
+	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i);
-		if (i <= '8')
+		putchar(i + '0');
+		if (i <= 8)
 		{
 			putchar(',');
 			putchar(' ');

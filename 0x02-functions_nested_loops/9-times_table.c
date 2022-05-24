3bf890128e226d6553f71f86b14109a7db7ff4ee
root <4905@holbertonschool.com>
2022-05-24 12:58:10 -0700

    Commit my 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * times_table - prints out the 9's times table....hopefully
+ *
+ * variable n1 is first operator
+ * variable n2 is second operator
+ * p is the product
+ *
+ * counting with _putchar is terrible once you go past one digit
+ * we have to think of it as divisors
+ * example 53 is two digits, so it needs two _putchars
+ * the first character is 53 / 10 - which is equal to 5 in integer division
+ * the second character is 53 % 10 - which is equal to 3 in modulus division
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int n1;
+	int n2;
+	int p;
+
+	for (n1 = 0; n1 <= 9; n1++)
+	{
+		for (n2 = 0; n2 <= 9; n2++)
+		{
+			p = n1 * n2;
+			if ((p / 10 == 0) && (p % 10 == 0))
+			{
+				_putchar((p % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				if (n2 <= 9)
+				{
+					_putchar(' ');
+					_putchar((p % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((p / 10) + '0');
+				_putchar((p % 10) + '0');
+				if (n2 < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			if (n2 == 9)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}

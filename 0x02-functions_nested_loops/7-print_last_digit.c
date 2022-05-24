a2fac5e5d64c22ebacddb0fdb306244e6347fb1f
root <4905@holbertonschool.com>
2022-05-24 11:41:34 -0700

    Committing 7-last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a provided integer
+ * @n: integer input
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	return (last);
+}

8170b17b5fea89b4aad52566f384554602887ab0
root <4905@holbertonschool.com>
2022-05-24 12:20:34 -0700

    Added comments to 8-24_hours.c to better explain counting

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,6 +3,14 @@
 /**
  * jack_bauer - prints every hour and minute for a day
  *
+ * variable h is for hours
+ * variable m is for minutes
+ * counting with _putchar is terrible once you go past one digit
+ * we have to think of it as divisors
+ * example 53 is two digits, so it needs two _putchars
+ * the first character is 53 / 10 - which is equal to 5 in integer division
+ * the second character is 53 % 10 - which is equal to 3 in modulus division
+ *
  * Return: void
  */
 void jack_bauer(void)

b019f1b19a4ef147c5ccb1df7e4d4f97b1ac22a8
root <4905@holbertonschool.com>
2022-05-24 17:38:21 -0700

    Made it so "," and " " only show up on the first 8 columns

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,8 +28,11 @@ void times_table(void)
 			p = n1 * n2;
 			_putchar((p / 10) + '0');
 			_putchar((p % 10) + '0');
+			if (n2 < 9)
+			{
 			_putchar(',');
 			_putchar(' ');
+			}
 			if (n2 == 9)
 			{
 				_putchar('\n');

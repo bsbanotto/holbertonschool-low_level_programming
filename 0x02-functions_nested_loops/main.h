9596d92f09a377498fd7620f73d07baafd7708c8
root <4905@holbertonschool.com>
2022-05-25 11:52:09 -0700

    Commit main.h that is guarded

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/* Prototypes */
+
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -21,3 +27,7 @@ void times_table(void);
 int add(int, int);
 
 void print_to_98(int n);
+
+
+
+#endif

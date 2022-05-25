ecc739a43fce5b22fccca5fedbcef4e129f62c32
root <4905@holbertonschool.com>
2022-05-24 17:29:45 -0700

    Modified comments

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,12 +13,6 @@
  * the first character is 53 / 10 - which is equal to 5 in integer division
  * the second character is 53 % 10 - which is equal to 3 in modulus division
  *
- * To build our table
- * First, for the zeroth row, give us a 0 , and ' '
- * Second, for every column, multiply the grid space, row * column
- * IF this is a single digit number, put in a space
- * If it's a two digit number, put in the tens place followed by the remainder
- * Lastly, for all columns but the last, we need a ', ' after each modulus
  * Return: void
  */
 void times_table(void)

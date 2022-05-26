0e80ad2f8b37a9de8ce8425cb9f5a8b2a46a7f8b
root <4905@holbertonschool.com>
2022-05-26 14:21:00 -0700

    Improved fix to compile error

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,14 +2,14 @@
 /**
  * main - Returns sum of all natural numbers that are a multiple of 3 or 5
  * below 1024 (excluded)
- * @i: is the counter
+ *
  *
  * Return: 0 when successful
  */
 
 int main(void)
 {
-	int sum;
+	int i, sum;
 
 	sum = 0;
 

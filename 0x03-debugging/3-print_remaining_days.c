c20c0842a508b77c723c53436b484a62b4abf73d
root <4905@holbertonschool.com>
2022-05-30 10:38:23 -0700

    Fixed a Betty

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -20,7 +20,7 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
-		if (month >=3)
+		if (month >= 3)
 		{
 			day = day + 2;
 			printf("Day of the year: %d\n", day);

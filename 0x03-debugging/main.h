f581abb3188928143ea3c844fa754c5df9af4d32
root <4905@holbertonschool.com>
2022-05-30 09:30:46 -0700

    Corrected typo on int largest_number function prototype

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 
 void positive_or_negative(int i);
 
-int largets_number(int a, int b, int c);
+int largest_number(int a, int b, int c);
 
 void print_remaining_days(int month, int day, int year);
 

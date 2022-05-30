d76abc302ca87316c2c0f44531bbed66ab03527e
root <4905@holbertonschool.com>
2022-05-30 10:41:34 -0700

    Still working on this leap year calculator

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,13 +16,12 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-			day++;
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
 		if (month >= 3)
 		{
-			day = day + 2;
+			day++;
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n ", 366 - day);
 		}

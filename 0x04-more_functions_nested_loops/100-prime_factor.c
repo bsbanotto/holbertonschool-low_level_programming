18d37d3c24f4f06f6908ee842986c1bf99a973d0
root <4905@holbertonschool.com>
2022-05-31 09:57:05 -0700

    Commit largest prime factor complete

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,22 +2,38 @@
 
 /**
  * main - prints largest prime of 612852475143
- *
+ * num: Number provided
+ * i: All factors
+ * j: Prime factor counter
+ * primeNum: prime numbers
  * Return: 0 when successful
  */
 
 int main(void)
 {
-	long num, i;
+	long num, i, j, prime;
 
-	num = 6000;
+	num = 612852475143;
+	prime = 1;
 
-	for (i = 2; i < num / 2; i++)
+	for (i = 2; i <= num / 2; i++)
 	{
 		if (num % i == 0) /* True when number is a factor */
 		{
-			printf("%ld, ", i);
+			/*printf("%ld\n", i);  Prints all factors */
+			for (j = 2; j < i / 2; j++)
+			{
+				if (i % j == 0)
+				{
+					if (j > prime)
+					{
+						prime = j;
+					}
+					/*printf("%ld\n", prime); */
+				}
+			}
 		}
 	}
+	printf("%ld\n", prime);
 	return (0);
 }

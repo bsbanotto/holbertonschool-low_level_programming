699055d90247b08210dc250f29ccd5ed54f6a4f3
root <4905@holbertonschool.com>
2022-06-01 13:07:18 -0700

    Submitting changes

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -19,16 +19,12 @@ void print_array(int *a, int n)
 	{
 		len++;
 	}
-
-	if (n > 0)
+	for (i = 0; i < n - 1 ; i++)
 	{
-		for (i = 0; i < n - 1; i++)
-		{
-			printf("%d, ", a[i]);
-		}
-		for (i = n - 1; i < n; i++)
-		{
-			printf("%d\n", a[i]);
-		}
+		printf("%d, ", a[i]);
+	}
+	for ( i = n - 1; i < n; i++)
+	{
+		printf("%d\n", a[i]);
 	}
 }

40c4441f400f1e29ce5469c4f57767d4c1c89409
root <4905@holbertonschool.com>
2022-06-01 12:53:42 -0700

    First commit of 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of numbers
+ * @a: given array
+ * @n: number of elements to print
+ *
+ * Return: Void
+ */
+
+void print_array(int *a, int n)
+{
+	int i, len;
+
+	len = 0;
+
+	while (a[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	for (i = n - 1; i < n; i++)
+	{
+		printf("%d\n", a[i]);
+	}
+}

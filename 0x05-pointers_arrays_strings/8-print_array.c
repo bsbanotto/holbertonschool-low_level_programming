69ea8648a2c1933747745aec675f9d4bb57a4db9
root <4905@holbertonschool.com>
2022-06-01 13:33:46 -0700

    Added check for array length must equal n

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -19,12 +19,15 @@ void print_array(int *a, int n)
 	{
 		len++;
 	}
-	for (i = 0; i < n - 1; i++)
+	if (len == n)
 	{
-		printf("%d, ", a[i]);
-	}
-	for (i = n - 1; i < n; i++)
-	{
-		printf("%d\n", a[i]);
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d, ", a[i]);
+		}
+		for (i = n - 1; i < n; i++)
+		{
+			printf("%d\n", a[i]);
+		}
 	}
 }

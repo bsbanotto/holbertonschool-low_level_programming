07c22158786d87584dc0b3fe2b670dee61ffd391
root <4905@holbertonschool.com>
2022-06-01 13:54:30 -0700

    Added test to make sure n is positive number

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,12 +13,15 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	if (n > 0)
 	{
-		printf("%d, ", a[i]);
-	}
-	for (i = n - 1; i < n; i++)
-	{
-		printf("%d\n", a[i]);
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d, ", a[i]);
+		}
+		for (i = n - 1; i < n; i++)
+		{
+			printf("%d\n", a[i]);
+		}
 	}
 }

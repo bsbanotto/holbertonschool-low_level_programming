7c6b4e435159a573695d1921d4803bae2eace4fb
root <4905@holbertonschool.com>
2022-06-01 09:55:12 -0700

    First commit of 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line
+ * @s: provided string
+ *
+ * Return: Void
+ */
+
+void print_rev(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (len = len; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}

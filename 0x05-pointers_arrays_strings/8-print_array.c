90b8a6eee86d13a4c84248fd2537b68fe9afdb89
root <4905@holbertonschool.com>
2022-06-01 13:50:10 -0700

    Took out length counter, did not need it

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,23 +11,14 @@
 
 void print_array(int *a, int n)
 {
-	int i, len;
+	int i;
 
-	len = 0;
-
-	while (a[len] != '\0')
+	for (i = 0; i < n - 1; i++)
 	{
-		len++;
+		printf("%d, ", a[i]);
 	}
-	if (len == n)
+	for (i = n - 1; i < n; i++)
 	{
-		for (i = 0; i < n - 1; i++)
-		{
-			printf("%d, ", a[i]);
-		}
-		for (i = n - 1; i < n; i++)
-		{
-			printf("%d\n", a[i]);
-		}
+		printf("%d\n", a[i]);
 	}
 }

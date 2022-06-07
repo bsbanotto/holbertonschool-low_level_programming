c6a0ba229551777c65b8d3436cffb8196064466f
root <4905@holbertonschool.com>
2022-06-07 07:17:40 -0700

    Commit directory, README, task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - Function that fills memory with a constant byte
+ *
+ * @s: Pointer to memory area
+ * @b: constant byte to write
+ * @n: number of bytes to fill
+ *
+ * Return: Pointer to memory area (s)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (n = n; n != 0; n--)
+	{
+		*s++ = b;
+	}
+	return (s);
+}

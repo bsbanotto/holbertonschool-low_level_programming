9b803634bf3c2398d42c6129f7dc87cf92537b68
root <4905@holbertonschool.com>
2022-06-07 13:52:28 -0700

    commit of chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,12 +2,21 @@
 
 /**
  * print_chessboard - Displays a chessboard
- * @a: size of grid
+ * @a: provided chessboard array
  *
  * Return: void
  */
 
 void print_chessboard(char (*a)[8])
 {
-	
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		_putchar('\n');
+	}
 }

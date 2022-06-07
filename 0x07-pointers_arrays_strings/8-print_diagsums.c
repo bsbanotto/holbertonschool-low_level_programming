6d100ba059912cd49fd3310af0e6496f5acfa328
root <4905@holbertonschool.com>
2022-06-07 16:02:07 -0700

    Committing add diagonale

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,20 +9,38 @@
 
 void print_diagsums(int *a, int size)
 {
-	int row, col;
+	int i;
 	int sum_tl_br = 0;
 	int sum_tr_bl = 0;
 
-	for (row = 0; row < size; row++)
-	{
-		sum_tl_br += a[row][row];
-	}
-	for (row = 0; row < size; row++)
-	{
-		for(col = size - 1; col >= 0; col--)
-		{
-			sum_tr_bl += a[row][col];
-		}
-	}
+/*
+ * For sum_tl_br - starts at top left, ends at bottom right
+ * first is zero, and go to size + 1 until counter = size
+ */
+	for (i = 0; i < size * size; i = i + size + 1)
+		sum_tl_br += a[i];
+/*
+ *	printf("top left to bottom right sum is: %d\n", sum_tl_br);
+ */
+
+/*
+ * For sum_tr_bl - starts at top right, ends at bottom left
+ * first i is size - 1, then i = i + size - 1
+ * run while i < size * size -1. If we do size * size, we get the value from
+ * the following row added in
+ */
+	for (i = size - 1; i < size * size - 1; i = i + size - 1)
+		sum_tr_bl += a[i];
+/*
+ *	printf("top right to bottom left sum is: %d\n", sum_tr_bl);
+ */
+
+/*
+ * I used this section to figure out how to return what I wanted
+ *	int i;
+ *	for(i = 0; i < size; i++)
+ *		printf("%d\n", size);
+ *	printf("%d\n", a[8]);
+ */
 	printf("%d, %d\n", sum_tl_br, sum_tr_bl);
 }

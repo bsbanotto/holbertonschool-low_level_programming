267a7b949c7cc9f32f790742351aca8233d744dc
root <4905@holbertonschool.com>
2022-06-07 11:25:21 -0700

    Commit task 1 counting up until n instead of down to 0 from n.

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	for (n = n; n != 0; n--)
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
 	{
-		*dest++ = *src;
-		src++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }

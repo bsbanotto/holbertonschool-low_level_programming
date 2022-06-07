068d9f92441cd9435b30466795e1d56ce3d9fc3d
root <4905@holbertonschool.com>
2022-06-07 08:36:43 -0700

    Commit of task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strchr - Locates a character in a string
+ * @s: Source string, where we look
+ * @c: Character we are looking for
+ *
+ * Return: pointer to first occurrance, or NULL if not in string
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != c && *s != '\0')
+	{
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	else
+		return ('\0');
+}

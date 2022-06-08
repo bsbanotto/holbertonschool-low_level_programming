5b1084272fa8e1ef1944c11b31c45a7e92833f28
root <4905@holbertonschool.com>
2022-06-07 18:01:21 -0700

    Added check for haystack to be shorter than needle

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,7 +12,13 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i, j, needleLen, hayLen;
+
+	needleLen = _strlen(needle);
+	hayLen = _strlen(haystack);
+
+	if (hayLen < needleLen)
+		return (NULL);
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
@@ -22,4 +28,20 @@ char *_strstr(char *haystack, char *needle)
 		}
 	}
 	return (NULL);
+}
+
+/**
+ * _strlen - Returns the integer length of a string
+ * @s: Supplied string
+ *
+ * Return: Length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
 }

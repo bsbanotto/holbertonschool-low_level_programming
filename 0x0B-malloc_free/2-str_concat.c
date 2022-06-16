c79ca285013a2f5c06e0481a118d09a71940bd20
root <4905@holbertonschool.com>
2022-06-16 08:33:48 -0700

    Should have read the directions to treat NULL strings as empty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,8 +16,10 @@ char *str_concat(char *s1, char *s2)
 	char *ar;
 	int i, j;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = ("");
+	if (s2 == NULL)
+		s2 = ("");
 
 	ar = malloc(_strlen(s1) + _strlen(s2) + 1);
 	if (ar == NULL)

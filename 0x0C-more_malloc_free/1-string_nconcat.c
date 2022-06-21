4b4dca47bacfb3141f0699140d113070ab7d0f90
root <4905@holbertonschool.com>
2022-06-21 13:12:07 -0700

    Commented out AR == NULL return NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j;
-	char *ar = NULL;
+	char *ar;
 
 	if (s2 == NULL)
 	{
@@ -31,11 +31,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 	ar = malloc(strlen(s1) + n + 1);
-	if (ar == NULL)
-	{
-		return (NULL);
-	}
-
+/*	if (ar == NULL)
+ *	{
+ *		return (NULL);
+ *	}
+ */
 	for (i = 0; i < strlen(s1); i++)
 	{
 		ar[i] = s1[i];

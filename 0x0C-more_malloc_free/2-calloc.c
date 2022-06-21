c167129d05f0b3b74fa86b18e54fe40d8e2e6c88
root <4905@holbertonschool.com>
2022-06-21 10:34:55 -0700

    Added my return NULL if malloc fails

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -23,7 +23,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	ar = malloc(nmemb * size);
 
-	for (i = 0; i <= nmemb * size; i++)
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb * size; i++)
 	{
 		ar[i] = 0;
 	}

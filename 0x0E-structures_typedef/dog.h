9ee0eca9aaa222624ceb7c08221e32f9df2f161d
root <4905@holbertonschool.com>
2022-06-22 09:00:57 -0700

    Consolidated functions and structs to dog.h, removed main.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,11 @@ struct dog
 	char *owner;
 };
 
+/* functio prototypes */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *pwner);
+void free_dog(dog_t *d);
+
 #endif

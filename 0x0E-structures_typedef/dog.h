5a49328f016927533242a6bb53aa38c363cf4306
root <4905@holbertonschool.com>
2022-06-22 07:58:53 -0700

    Added double inclusion protection and Betty doc to dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines dog traits
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: This structure gives a dog's name, age, and owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif

b23d569ac8ecbe1773bcd37f6e636b0e8b90a22e
root <4905@holbertonschool.com>
2022-06-23 13:39:09 -0700

    Commit task 3, did not test null operator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,75 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Sum of two integers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - Subtracts two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Difference of two integers
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - Multiplies two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Product of two integers
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - Dividestwo integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Division of two integers
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - modulo of two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Remainder of two divided integers
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

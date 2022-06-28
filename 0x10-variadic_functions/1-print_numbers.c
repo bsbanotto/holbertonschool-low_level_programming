4a45dfb25d3bfd7197567fdc223485d2e3e5c38e
bsbanotto <4905@holbertonschool.com>
2022-06-28 10:45:28 -0700

    Lots of changes to task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,18 +21,19 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (n > 0)
 	{
 
-		if (separator == NULL)
-		{
-			for (i = 0; i < n; i++)
-			{
-				printf("%d", va_arg(numbers, unsigned int));
-			}
-		}
-		else
+		if (separator != NULL)
 		{
 			for (i = 0; i < (n - 1); i++)
 			{
 				printf("%d%s", va_arg(numbers, unsigned int), separator);
+			}
+			printf("%d", va_arg(numbers, unsigned int));
+		}
+		else
+		{
+			for (i = 0; i < n; i++)
+			{
+				printf("%d", va_arg(numbers, unsigned int));
 			}
 		}
 		printf("\n");

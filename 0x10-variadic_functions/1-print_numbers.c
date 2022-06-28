c517dd54811646f7545edbc0a759ab9ff29e711f
bsbanotto <4905@holbertonschool.com>
2022-06-28 10:40:05 -0700

    Changed second if to else

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -28,7 +28,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 				printf("%d", va_arg(numbers, unsigned int));
 			}
 		}
-		if (separator != NULL)
+		else
 		{
 			for (i = 0; i < (n - 1); i++)
 			{

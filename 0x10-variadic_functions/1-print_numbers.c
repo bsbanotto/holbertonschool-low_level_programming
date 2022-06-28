e37ab7c0ce18d0bf377cba79668a677d1c2bc9ce
bsbanotto <4905@holbertonschool.com>
2022-06-28 10:26:31 -0700

    Changed where for loops start

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,14 +23,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		if (separator == NULL)
 		{
-			for (i = 1; i < n; i++)
+			for (i = 0; i < n; i++)
 			{
 				printf("%d", va_arg(numbers, int));
 			}
 		}
 		if (separator != NULL)
 		{
-			for (i = 1; i < n; i++)
+			for (i = 0; i < n; i++)
 			{
 				printf("%d%s", va_arg(numbers, int), separator);
 			}

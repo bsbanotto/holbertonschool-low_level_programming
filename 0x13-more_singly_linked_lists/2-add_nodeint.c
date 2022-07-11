6ff6159b685c33d0a87cba55fa2ab45c32e92c41
bsbanotto <4905@holbertonschool.com>
2022-07-11 07:27:08 -0700

    First commit for task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * *add_nodeint - Adds note to start of integer list
+ * @head: double pointer to head of list
+ * @n: integer to add
+ * 
+ * Return: Address of new element, or NULL if failure
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+    listint_t *temp;
+
+    temp = malloc(sizeof(listint_t));
+    if (temp == NULL)
+        return(NULL);
+
+    temp->n = n;
+    temp->next = *head;
+    *head = temp;
+    return (temp);
+}

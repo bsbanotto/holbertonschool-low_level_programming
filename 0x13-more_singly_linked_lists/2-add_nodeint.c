f146ac2a9f337510710cb3de33f8e1087834e97b
bsbanotto <4905@holbertonschool.com>
2022-07-11 07:44:13 -0700

    Betty fixes, spaces to tabs

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,20 +4,20 @@
  * *add_nodeint - Adds note to start of integer list
  * @head: double pointer to head of list
  * @n: integer to add
- * 
+ *
  * Return: Address of new element, or NULL if failure
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *temp;
+	listint_t *temp;
 
-    temp = malloc(sizeof(listint_t));
-    if (temp == NULL)
-        return(NULL);
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (NULL);
 
-    temp->n = n;
-    temp->next = *head;
-    *head = temp;
-    return (temp);
+	temp->n = n;
+	temp->next = *head;
+	*head = temp;
+	return (temp);
 }

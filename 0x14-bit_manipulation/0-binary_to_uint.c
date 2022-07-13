5addefd601ec907d6a7d370d84ce3363eb66d88e
bsbanotto <4905@holbertonschool.com>
2022-07-13 10:12:52 -0700

    Added check for b == NULL

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = i - 1; i >= 0; i--)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if ((b[i] != '0' && b[i] != '1') || b == NULL)
 			return (0);
 		if (b[i] == '1')
 			value += base;

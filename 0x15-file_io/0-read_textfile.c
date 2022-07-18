241f869ddec8d139ba639f35c277bd3748cbc308
bsbanotto <4905@holbertonschool.com>
2022-07-18 08:09:58 -0700

    Betty fixes for task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,17 +19,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return(0);
+		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
 	{
-		close (fd);
+		close(fd);
 		return (0);
 	}
 
 	lenread = read(fd, buf, letters);
 	close(fd);
-	if(lenread == -1)
+	if (lenread == -1)
 	{
 		free(buf);
 		return (0);

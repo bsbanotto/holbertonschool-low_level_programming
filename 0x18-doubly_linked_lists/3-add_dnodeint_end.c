a7964c68210912e9715dc30b7026aa470ea07482
bsbanotto <4905@holbertonschool.com>
2022-08-02 06:58:43 -0700

    Added check that head points to valid linked list

--- 0x18-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,6 +12,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new, *temp;
 
+	if (!head)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	temp = malloc(sizeof(dlistint_t));
 	if (new == NULL || temp == NULL)

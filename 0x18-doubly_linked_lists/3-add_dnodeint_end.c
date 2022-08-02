f53b8e5bf71efe0985b14d8ae1b8cce39a1e0256
bsbanotto <4905@holbertonschool.com>
2022-08-02 11:31:01 -0700

    git add

--- 0x18-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,9 +24,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = new;
 		return (new);
 	}
-	temp = malloc(sizeof(dlistint_t));
-	if (temp == NULL)
-		return (NULL);
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
